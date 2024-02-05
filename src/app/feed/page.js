e0677e404e428b05545e82919b31091b576490b4
Htet Lin <htetlin099798@gmail.com>
2024-02-05 10:38:58 +0630

    feat: add initial structure for left bar

--- src/app/feed/page.js
+++ src/app/feed/page.js
@@ -2,12 +2,13 @@
 
 import React from "react";
 import { signOut } from "next-auth/react";
+import LeftBar from "./(leftBar)/LeftBar";
 
 const Feed = () => {
   return (
     <div className="flex justify-center w-full bg-slate-200">
       {/* left nav bar */}
-      <div className="fixed top-0 left-0 w-full h-full bg-gray-300 max-w-96"></div>
+      <LeftBar />
       {/* main content */}
       <main className="w-full h-full max-w-[680px] min-h-[200vh] bg-red-300"></main>
       {/* right bar */}
