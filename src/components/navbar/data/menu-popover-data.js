9fe381b618f644e40465afceddcf225379c8a19e
Htet Lin <htetlin099798@gmail.com>
2024-02-11 10:33:06 +0630

    feat: add menu popover data

---
+++ src/components/navbar/data/menu-popover-data.js
@@ -0,0 +1,128 @@
+export default menuPopoverData = [
+  {
+    group: "Social",
+    elements: [
+      {
+        title: "Events",
+        description:
+          "Organize or find events and other things to do online and nearby.",
+        iconUrl: "./public/assets/icons/events-icon",
+      },
+      {
+        title: "Find friends",
+        description: "Search for friends or people you my know.",
+        iconUrl: "./public/assets/icons/find-friends-icon",
+      },
+      {
+        title: "Groups",
+        description: "Connect with people who share your interests.",
+        iconUrl: "./public/assets/icons/groups-icon",
+      },
+      {
+        title: "News Feed",
+        description: "See relevant posts from people and Pages you follow.",
+        iconUrl: "./public/assets/icons/news-feed-icon",
+      },
+      {
+        title: "Feeds",
+        description:
+          "See the most recent posts from  your friends, groups, Pages and more.",
+        iconUrl: "./public/assets/icons/feeds-icon",
+      },
+      {
+        title: "Pages",
+        description: "Discover and connect with business on Facebook.",
+        iconUrl: "./public/assets/icons/pages-icon",
+      },
+    ],
+  },
+  {
+    group: "Entertainment",
+    elements: [
+      {
+        title: "Gaming Video",
+        description:
+          "Watch and connect with your favorite games and streamers.",
+        iconUrl: "./public/assets/icons/gaming-video-icon",
+      },
+      {
+        title: "Play games",
+        description: "Play your favorite games.",
+        iconUrl: "./public/assets/icons/play-games-icon",
+      },
+      {
+        title: "Video",
+        description:
+          "A video destination personalized to your interests and connections.",
+        iconUrl: "./public/assets/icons/video-icon",
+      },
+    ],
+  },
+  {
+    group: "Shopping",
+    elements: [
+      {
+        title: "Orders and payments",
+        description:
+          "A seamless, secure way to pay on the apps you already use.",
+        iconUrl: "./public/assets/icons/orders-icon",
+      },
+    ],
+  },
+  {
+    group: "Personal",
+    elements: [
+      {
+        title: "Recent ad activity",
+        description: "See all the ads you interacted with on Facebook.",
+        iconUrl: "./public/assets/icons/recent-ad-icon",
+      },
+      {
+        title: "Memories",
+        description: "Browse your old photos, videos and posts  on Facebook.",
+        iconUrl: "./public/assets/icons/memories-icon",
+      },
+      {
+        title: "Saved",
+        description: "Find posts, photos and videos that you saved for later.",
+        iconUrl: "./public/assets/icons/saved-icon",
+      },
+    ],
+  },
+  {
+    group: "Professional",
+    elements: [
+      {
+        title: "Ads Manager",
+        description: "Create, manage and track the performance of you ads.",
+        iconUrl: "./public/assets/icons/ads-manager-icon",
+      },
+    ],
+  },
+  {
+    group: "Community Resources",
+    elements: [
+      {
+        title: "Climate Science Center",
+        description: "Learn about climate change and its effects.",
+        iconUrl: "./public/assets/icons/climate-icon",
+      },
+      {
+        title: "Fundraisers",
+        description:
+          "Donate and raise money for nonprofits and  personal causes",
+        iconUrl: "./public/assets/icons/fundraisers-icon",
+      },
+    ],
+  },
+  {
+    group: "More from Meta",
+    elements: [
+      {
+        title: "Messenger Kids",
+        description: "Let kids message with close friends and family.",
+        iconUrl: "./public/assets/icons/messenger-kids-icon",
+      },
+    ],
+  },
+];
