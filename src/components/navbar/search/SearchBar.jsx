2e59412a9cf439af1f932ce6479056f25ca50b29
Htet Lin <htetlin099798@gmail.com>
2024-02-12 13:06:10 +0630

    refactor: put searchField into component folder

--- src/components/navbar/search/SearchBar.jsx
+++ src/components/navbar/search/SearchBar.jsx
@@ -1,14 +1,9 @@
-import { Input } from "@/components/ui/input";
+import SearchField from "@/components/SearchField";
+
 import React from "react";
-import { FiSearch } from "react-icons/fi";
 
 const SearchBar = () => {
-  return (
-    <div className="relative flex items-center ">
-      <FiSearch className="absolute w-4 h-4 transform -translate-y-1/2 left-2 top-1/2 text-muted-foreground" />
-      <Input placeholder="Your search..." className="pl-8 " />
-    </div>
-  );
+  return <SearchField />;
 };
 
 export default SearchBar;
