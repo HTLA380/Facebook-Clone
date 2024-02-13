d9305479564553fbe20c0baff38058f2b64343ae
Htet Lin <htetlin099798@gmail.com>
2024-02-13 15:20:49 +0630

    feat: add user messenger popover data

---
+++ src/components/navbar/data/messenger-popover-data.js
@@ -0,0 +1,12 @@
+import { faker } from "@faker-js/faker";
+
+export const messengerPopoverData = [...Array(24)].map((_, index) => ({
+  id: faker.string.uuid(),
+  avatarUrl: `./assets/avatars/avatar_${index + 1}.jpg`,
+  userName: faker.person.fullName(),
+  message: faker.lorem.sentence(),
+  isUserLastMessage: faker.datatype.boolean(),
+  isSeenLastMessage: faker.datatype.boolean(),
+  isOnLine: faker.datatype.boolean(),
+  date: faker.date.past(),
+}));
