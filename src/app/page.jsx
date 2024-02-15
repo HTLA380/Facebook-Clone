b2d86739b9989cf57280f2322a81ffb5f69e41c0
Htet Lin <htetlin099798@gmail.com>
2024-02-15 09:56:38 +0630

    fix: resolve url issue

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Feed from "./(feed)/page";
 
-const HomePage = async () => {
+const HomePage = () => {
+  useEffect(() => {
+    if (window.location.hash == "#_=_") {
+      window.location.hash = "";
+    }
+  }, []);
+
   return <Feed />;
 };
 
