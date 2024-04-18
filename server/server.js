92a8369200c308f74420d21b0feba6fb7a2de6b0
Htet Aung Lin <htetlin099798@gmail.com>
2024-04-19 06:28:17 +0630

    feat(backend): add error handler middleware

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const notifications = require("./routes/notifications");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const PORT = 8080;
 
 app.use("/api/notifications", notifications);
+
+app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
