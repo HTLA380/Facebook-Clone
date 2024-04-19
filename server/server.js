41786562be9959aa59576c5ce0e117b7f7bfea80
Htet Aung Lin <htetlin099798@gmail.com>
2024-04-19 06:34:20 +0630

    feat(backend): add not found error handler

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const app = express();
 const notifications = require("./routes/notifications");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 const dotenv = require("dotenv");
+const notFound = require("./middlewares/not-found");
 dotenv.config();
 
 const PORT = 8080;
 
 app.use("/api/notifications", notifications);
 
+app.use(notFound);
 app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
