9806a00a5887da89431ba9808c6b020b17ad9e66
Htet Aung Lin <htetlin099798@gmail.com>
2024-05-17 15:55:52 +0630

    feat: removed backend side

--- server/middlewares/error-handler.js
+++
@@ -1,8 +0,0 @@
-const errorHandlerMiddleware = (err, req, res, next) => {
-  res.status(500).json({
-    success: false,
-    msg: err.message,
-  });
-};
-
-module.exports = errorHandlerMiddleware;
